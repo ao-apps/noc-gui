d2c21aac80fb50ba279fcca19be338faaf095551
AO Industries, Inc <support@aoindustries.com>
2023-08-27 15:48:50 -0500

    TODO: Security manager deprecated in Java 17

--- src/main/java/com/aoindustries/noc/gui/Noc.java
+++ src/main/java/com/aoindustries/noc/gui/Noc.java
@@ -1,6 +1,6 @@
 /*
  * noc-gui - Graphical User Interface for Network Operations Center.
- * Copyright (C) 2007-2013, 2016, 2017, 2018, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2007-2013, 2016, 2017, 2018, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -100,6 +100,7 @@ public class Noc {
   @SuppressWarnings({"UseSpecificCatch", "TooBroadCatch", "UseOfSystemOutOrSystemErr"})
   public static void main(String[] args) {
     try {
+      // TODO: Security manager deprecated in Java 17
       if (System.getSecurityManager() == null) {
         System.setSecurityManager(new SecurityManager());
       }
